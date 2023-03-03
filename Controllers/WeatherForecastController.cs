6fda92592153515826627dbbf89d45c98acebd97
Anees Babu <aneesbabulianars007@gmail.com>
2023-03-03 18:12:45 +0530

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly HttpClient httpClient;
+        public WeatherForecastController(HttpClient client)
+        {
+            httpClient = client;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // This is a simple custom header custom headers
+            Response.Headers.Add("name", "This is my name");
+            Response.Headers.Add("No", "111");
+            // Return response
+            return Ok(new List<string> { "Dotnet", "Angular", "React" });
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] MyModel model)
+        {
+            
+            // Add custom headers
+            Response.Headers.Add("Dotnet", "C#");
+            Response.Headers.Add("Angular", "TS");
+            Response.Headers.Add("React", "JS");
+
+
+            // Process request
+            // ...
+            // Return response
+            return Ok(new { success = true });
+        }
+    }
+    public class MyModel
+    {
+        public string Property1 { get; set; }
+        public int Property2 { get; set; }
+    }
+}
+
